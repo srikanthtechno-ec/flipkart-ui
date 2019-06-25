dea62cdaa495935d83a48fd5ef5a241105e470a0
unknown <srikanth.shukrith@gmail.com>
2019-06-25 20:32:08 +0530

    removed some lines for conflicts

--- firstexample.js
+++ firstexample.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-Date is 6/22/2019
-=======
-I am in newmaster
->>>>>>> newmaster
-=======
-Newmaster
->>>>>>> newmaster
 Hi
 This file js file
 this should be in the ui repo
